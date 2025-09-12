1fb9f7965dc10cdef688fba8d7cd3e680e68f882
Khushi Patel <khushiiip2004@gmail.com>
2025-09-12 20:13:12 +0530

    change the color of branch field

--- studentapp/studentappurls.py
+++ studentapp/studentappurls.py
@@ -1,19 +1,4 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns=[
-#     path('studenthome/',views.studenthome,name='studenthome'),
-#     path('studentlogout/',views.studentlogout,name='studentlogout'),
-#     path('response/',views.response,name='response'),
-#     path('postquestion/',views.postquestion,name='postquestion'),
-#     path('postanswer/<qid>',views.postanswer,name='postanswer'),
-#     path('postans/',views.postans,name='postans'),
-#     path('viewanswer/<qid>',views.viewanswer,name='viewanswer'),
-#     path('changepassword/',views.changepassword,name='changepassword'),
-#     path('viewmat/',views.viewmat,name='viewmat'),
-#     path('viewprofile/',views.viewprofile,name='viewprofile'),
-# ]
-# studentapp/urls.py
+ 
 from django.urls import path
 from . import views
 
