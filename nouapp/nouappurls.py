d3b6f3b3701f14d530bbe2f57a28e865afb9813e
hruthikachavan <hruthikachavan295@gmail.com>
2025-10-18 09:15:23 +0530

    created the resources file and added path in nav

--- nouapp/nouappurls.py
+++ nouapp/nouappurls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path('contactus/', views.contactus, name='contactus'),
     path('courses/', views.courses, name='courses'),
     path('services/', views.services, name='services'),
+     path('resources/', views.resources, name='resources'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('reset-password/<str:token>/', views.reset_password, name='reset_password'),
     path('chat/', views.chat_api, name='chat_api'),  # added from other branch,
     path('search/', views.search_view, name='search'),
 ]
+
